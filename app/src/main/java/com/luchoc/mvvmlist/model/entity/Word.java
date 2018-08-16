1dc4087c84685b26e033ee287381b1b31f9cb1d7
Luis Carlin <luiscarlin@gmail.com>
2018-08-16 06:51:22 -0400

    Added entity

---
+++ app/src/main/java/com/luchoc/mvvmlist/model/entity/Word.java
@@ -0,0 +1,40 @@
+package com.luchoc.mvvmlist.model.entity;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+// All room annotations found here => https://developer.android.com/reference/android/arch/persistence/room/package-summary
+
+// Entity = table in database
+// if no tableName is provided, name of the class will be the name of the table
+@Entity(tableName = "word_table")
+public class Word {
+
+    /**
+     * You can auto-generate private keys (ids) as follows
+     * @PrimaryKey(autoGenerate = true)
+     * private int id;
+     */
+
+    // word becomes primary key (cannot be duplicated).
+    @PrimaryKey
+    // Cannot be null
+    @NonNull
+    // name of the column in the DB will be word
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public Word(@NonNull String word) {
+        this.mWord = word;
+    }
+
+    // mWord needs to be either public or have a getter
+    public String getWord(){
+        return this.mWord;
+    }
+}
+
+
+
